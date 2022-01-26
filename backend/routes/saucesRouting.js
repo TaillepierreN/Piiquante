05e2898b842fe11bc979ce46752e9e5f80c5913d
Nicolas Taillepierre <taillepierren@gmail.com>
2022-01-26 09:01:45 +0100

    ajout multer

--- backend/routes/saucesRouting.js
+++ backend/routes/saucesRouting.js
@@ -1,16 +1,17 @@
 // 6  routes
 const {Router} = require('express');
-const { displaySauces, displaySauce, postSauce, updateSauce, deleteSauce, likeSauce } = require('../controllers/saucesController');
+const { displaySauces, displaySauce, newSauce, updateSauce, deleteSauce, likeSauce } = require('../controllers/saucesController');
 const auth = require('../middlewares/auth');
 const router = Router();
+const multer = require('../middlewares/multer-config')
 
 router.get('/', auth, displaySauces)
 
 router.get('/:id', auth, displaySauce)
 
-router.post('/', auth, postSauce)
+router.post('/', auth, multer, newSauce)
 
-router.put('/:id', auth, updateSauce)
+router.put('/:id', auth, multer, updateSauce)
 
 router.delete('/:id', auth, deleteSauce)
 
