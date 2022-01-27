5ae173828549c5a8b15f730a38c6c2708fa1fe14
Nicolas Taillepierre <taillepierren@gmail.com>
2022-01-27 14:40:39 +0100

    ajout authparams regex

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -37,37 +37,6 @@ exports.signup = async (req, res) => {
  */
 
 exports.login = async (req, res) => {
-    // User.findOne({
-    //         email: req.body.email
-    //     })
-    //     .then(user => {
-    //         if (!user) {
-    //             return res.status(404).json({
-    //                 error: "Utilisateur non trouvé"
-    //             });
-    //         }
-    //         bcrypt.compare(req.body.password, user.password)
-    //             .then(valid => {
-    //                 if (!valid) {
-    //                     return res.status(400).json({
-    //                         error: "Mot de passe incorrect"
-    //                     });
-    //                 }
-    //                 res.status(200).json({
-    //                     userId: user._id,
-    //                     token: jwt.sign({
-    //                             userId: user._id
-    //                         },
-    //                         process.env.TOKEN_SECRET, {
-    //                             expiresIn: '24h'
-    //                         }
-    //                     )
-    //                 });
-    //             })
-    //             .catch(error => res.status(500).json({
-    //                 error
-    //             }));
-    //     })
     try {
         const user = await User.findOne({
             email: req.body.email
